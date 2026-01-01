6644551d4fb7f4af20e76edc136d5ec64b0ccdfc
Udesh <udesh.bhatti123@gmail.com>
2026-01-01 20:28:32 +0530

    Worked on mongodb schema and connection

---
+++ src/lib/db.ts
@@ -0,0 +1,36 @@
+import { connect } from "mongoose";
+import mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGO_URI
+
+if(!MONGO_URI){
+  throw new Error('Mongo url is not found')
+}
+
+let cached = global.mongoose
+
+if(!cached){
+    cached = global.mongoose ={ conn:null , promise:null }
+}
+
+const connectDb = async () =>{
+  if(cached.conn){
+    return cached.conn
+  }
+
+  if(!cached.promise){
+   cached.promise =  connect(MONGO_URI).then((c)=>c.connection)
+  }
+
+
+  try{
+        cached.conn = await cached.promise
+  }catch (error){
+        throw error
+  }
+
+  return cached.conn
+
+}
+
+export default connectDb    
